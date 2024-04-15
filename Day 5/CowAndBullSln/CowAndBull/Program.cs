b26ad7628e2f8827ca040ca2c355c2ca2d351265
Gokul K <gokulkumar5505@gmail.com>
2024-04-15 16:25:38 +0530

    Day 5 Progress

---
+++ Day 5/CowAndBullSln/CowAndBull/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CowAndBull
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cow and Bull Game!");
+            string word = "golf";
+
+            bool guessed = false;
+            int attempts = 0;
+
+            while (!guessed)
+            {
+                Console.WriteLine($"Attempt {++attempts}");
+                Console.Write("Enter your guess: ");
+                string guess = Console.ReadLine().ToLower();
+
+                if (guess.Length != 4)
+                {
+                    Console.WriteLine("Please enter a 4-letter word.");
+                    continue;
+                }
+
+                int cows = 0;
+                int bulls = 0;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == guess[i])
+                    {
+                        cows++;
+                    }
+                    else if (word.Contains(guess[i]))
+                    {
+                        bulls++;
+                    }
+                }
+
+                Console.WriteLine($"Cows: {cows}, Bulls: {bulls}");
+
+                if (cows == 4)
+                {
+                    guessed = true;
+                }
+            }
+
+            Console.WriteLine($"Congratulations!!! You guessed the word '{word}' in {attempts} attempts.");
+        }
+    }
+}
---
+++ Day 5/UnderstandingSeqSln/UnderstandingSeq/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace UnderstandingSeq
+{
+    public class Program
+    {
+        static void UnderstandingSwitch() 
+        {
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 0:
+                    Console.WriteLine("You have choosen " + n);
+                    break;
+                case 1:
+                    Console.WriteLine("You have choosen " + n);
+                    break;
+                case 2:
+                    Console.WriteLine("You have choosen " + n);
+                    break;
+                case 3:
+                    Console.WriteLine("You have choosen " + n);
+                    break;
+                default:
+                    Console.WriteLine("You have choosen " + n);
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Hello, World!");
+            program.UnderstandingSwitch();
+        }
+    }
+}
