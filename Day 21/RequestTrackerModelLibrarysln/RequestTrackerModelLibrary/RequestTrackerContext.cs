2497f8b2b85b209ede84c798db8ef9aa90f4556b
Gokul K <gokulkumar5505@gmail.com>
2024-05-09 14:09:45 +0530

    Day 21 Driver Gokul & Navigator Senthamil

---
+++ Day 21/RequestTrackerModelLibrarysln/RequestTrackerModelLibrary/RequestTrackerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerModelLibrary
+{
+	public class RequestTrackerContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=G3SLAPTOP\SQLEXPRESS;Integrated Security=true;Initial Catalog=dbEmployeeTrackerCF;");
+		}
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Request> Requests { get; set; }
+	}
+}
