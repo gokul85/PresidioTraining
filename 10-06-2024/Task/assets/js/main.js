140a307f583eb7cc3b6774183d8af66762fa08db
Gokul K <gokulkumar5505@gmail.com>
2024-06-13 16:45:11 +0530

    Day 46 Morning Class Work and Assignment

---
+++ 10-06-2024/Task/assets/js/main.js
@@ -0,0 +1,20 @@
+function showToast(message, type) {
+    const toastContainer = document.getElementById('toast-container');
+    const toast = document.createElement('div');
+    toast.className = `toast ${type}`;
+    toast.textContent = message;
+
+    toastContainer.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+
+        setTimeout(() => {
+            toastContainer.removeChild(toast);
+        }, 300);
+    }, 3000);
+}
